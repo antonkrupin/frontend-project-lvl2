a6163d11d1e24eae40cec3884796cd7e6bf463ad
Anton Krupin <krupin_anton@mail.ru>
2021-11-24 11:01:42 +0300

    change gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,7 +1,5 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
-
 const program = new Command();
-program
-    .option('-d, --debug', 'output extra');
-console.log('test');
+
+console.log('test')
