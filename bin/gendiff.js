d9ff059b24a36f3ebf1d296548be9cf7790b78c5
Anton Krupin <krupin_anton@mail.ru>
2021-11-24 11:31:35 +0300

    add help information

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,11 +3,7 @@ import { Command } from 'commander';
 const program = new Command();
 
 program
-  .version('0.0.1', '-V, --version', 'output the version number')
-  .description('Compares two configuration files and shows a difference.')
-  .option('-f, --format [type]', 'output format', 'stylish')
-  .arguments('<filepath1> <filepath2>')
-  .action((filepath1, filepath2, { format }) => {
-    console.log(genDiff(filepath1, filepath2, format));
-  });
+  .description('Compares two configuration files and shows a difference')
+  .option('-V, --version', 'output the version number');
+
 program.parse();
