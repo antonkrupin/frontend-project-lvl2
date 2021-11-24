f23f36b794ef4f924d037a5aaddedc36d06e71f5
Anton Krupin <krupin_anton@mail.ru>
2021-11-24 11:58:47 +0300

    add arguments, customize auto help output

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -2,8 +2,16 @@
 import { Command } from 'commander';
 const program = new Command();
 
+program.configureHelp({
+    sortOptions: false,
+});
+
 program
   .description('Compares two configuration files and shows a difference')
-  .option('-V, --version', 'output the version number');
+  .option('-V, --version', 'output the version number')
+  .helpOption('-h, --help', 'output usage information')
+  .option('-f, --format', 'output format')
+  .argument('<pathToFile 1>', 'enter path to file 1')
+  .argument('<pathToFile 2>', 'enter path to file 2');
 
 program.parse();
