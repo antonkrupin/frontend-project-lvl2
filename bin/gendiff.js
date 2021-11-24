a798db612ef7fb177415de4a6139aabf41124ed8
Anton Krupin <krupin_anton@mail.ru>
2021-11-24 11:07:04 +0300

    change

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -2,4 +2,12 @@
 import { Command } from 'commander';
 const program = new Command();
 
-console.log('test')
+program
+  .version('0.0.1', '-V, --version', 'output the version number')
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format [type]', 'output format', 'stylish')
+  .arguments('<filepath1> <filepath2>')
+  .action((filepath1, filepath2, { format }) => {
+    console.log(genDiff(filepath1, filepath2, format));
+  });
+program.parse();
