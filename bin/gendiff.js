7623a80ceee903c1a58dc5f4f7b55eb188bad214
Anton Krupin <krupin_anton@mail.ru>
2021-11-24 16:24:51 +0300

    remove for cycle, add map function

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -32,9 +32,9 @@ program
 
     const keysUnion = _.union(Object.keys(file1JSON), Object.keys(file2JSON)).sort();
 
-    const diffArray = [];
+    //const diffArray = [];
 
-    for (let el of keysUnion) {
+    /*for (let el of keysUnion) {
         if (_.has(file1JSON, el) && _.has(file2JSON, el)) {
             if (file1JSON[el] === file2JSON[el]) {
                 diffArray.push('  ' + el + ': ' + file1JSON[el])
@@ -47,7 +47,21 @@ program
         } else if (_.has(file2JSON, el)) {
             diffArray.push('+ ' + el + ': ' + file2JSON[el]);
         }
-    }
+    }*/
+
+    const diffArray = keysUnion.map((el) => {
+        if (_.has(file1JSON, el) && _.has(file2JSON, el)) {
+            if (file1JSON[el] === file2JSON[el]) {
+                return ('  ' + el + ': ' + file1JSON[el])
+            } else {
+                return ([('- ' + el + ': ' + file1JSON[el]), ('+ ' + el + ': ' + file2JSON[el])].join('\n'))
+            }
+        } else if (_.has(file1JSON, el)) {
+            return ('- ' + el + ': ' + file1JSON[el]);
+        } else if (_.has(file2JSON, el)) {
+            return ('+ ' + el + ': ' + file2JSON[el]);
+        }
+    });
 
     return diffArray.join('\n');
 });
