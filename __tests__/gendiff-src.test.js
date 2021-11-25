f8804cdda4a3c9b8e7a190a80bff41991da04764
Anton Krupin <krupin_anton@mail.ru>
2021-11-25 12:08:40 +0300

    change

--- __tests__/gendiff-src.test.js
+++ __tests__/gendiff-src.test.js
@@ -5,9 +5,8 @@ test('gendiffString', () => {
     const path1 = 'file1.json';
     const path2 = 'file2.json';
     const getFixturePath = (filename) => path.join('..', '__fixtures__', filename);
-    const test = getFixturePath(path1);
-    console.log(test)
-    //const stringTest = ('- follow: false\n  host: hexlet.io\n- proxy: 123.234.53.22\n- timeout: 50\n+ timeout: 20\n+ verbose: true')
+    const file1 = getFixturePath(path1);
+    const file2 = getFixturePath(path2);
     const test = ['- follow: false', '  host: hexlet.io', '- proxy: 123.234.53.22', '- timeout: 50', '+ timeout: 20', '+ verbose: true'].join('\n')
-    expect(gendiffString(test, path2).join('\n')).toEqual(test);
+    expect(gendiffString(file1, file2).join('\n')).toEqual(test);
 });
