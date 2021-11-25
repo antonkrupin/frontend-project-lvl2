9c608920f35fc3b4f4dc3f4599cc889a2f754a1e
Anton Krupin <krupin_anton@mail.ru>
2021-11-25 07:40:44 +0300

    remove import lodash, fs. remove function readFile, diffArray from action

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
-import { readFileSync } from 'fs';
-import _ from 'lodash';
+import gendiff from '../src/gendiff-src.js';
 
 const program = new Command();
 
@@ -17,36 +16,7 @@ program
   .argument('<pathToFile1>', 'enter path to file 1')
   .argument('<pathToFile2>', 'enter path to file 2')
   .action((pathToFile1, pathToFile2) => {        
-
-    const readFile = (pathToFile) => {
-        return readFileSync(pathToFile, 'utf-8', (err) => {
-            if (err) {
-                console.log(err);
-                return;
-            }
-        });
-    };
-
-    const file1JSON = JSON.parse(readFile(pathToFile1))
-    const file2JSON = JSON.parse(readFile(pathToFile2))
-
-    const sortedKeysUnion = _.union(Object.keys(file1JSON), Object.keys(file2JSON)).sort();
-
-    const diffArray = sortedKeysUnion.map((el) => {
-        if (_.has(file1JSON, el) && _.has(file2JSON, el)) {
-            if (file1JSON[el] === file2JSON[el]) {
-                return ('  ' + el + ': ' + file1JSON[el])
-            } else {
-                return ([('- ' + el + ': ' + file1JSON[el]), ('+ ' + el + ': ' + file2JSON[el])].join('\n'))
-            }
-        } else if (_.has(file1JSON, el)) {
-            return ('- ' + el + ': ' + file1JSON[el]);
-        } else if (_.has(file2JSON, el)) {
-            return ('+ ' + el + ': ' + file2JSON[el]);
-        }
-    });
-
-    return diffArray.join('\n');
+    console.log(gendiff(pathToFile1, pathToFile2));
 });
 
 program.parse();
