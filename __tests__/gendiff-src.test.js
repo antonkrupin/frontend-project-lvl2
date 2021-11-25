2a25ef6ac0da9dd0af3a8d70d6d8c48e9e5c39a6
Anton Krupin <krupin_anton@mail.ru>
2021-11-25 09:02:58 +0300

    change test

--- __tests__/gendiff-src.test.js
+++ __tests__/gendiff-src.test.js
@@ -3,6 +3,6 @@ import gendiffString from "../src/gendiff-src.js";
 test('gendiffString', () => {
     const path1 = 'file1.json';
     const path2 = 'file2.json';
-    const stringTest = ('- follow: false\n   host: hexlet.io\n - proxy: 123.234.53.22\n - timeout: 50\n + timeout: 20\n + verbose: true\n')
+    const stringTest = ('- follow: false\n   host: hexlet.io\n - proxy: 123.234.53.22\n - timeout: 50\n + timeout: 20\n + verbose: true')
     expect(gendiffString(path1, path2)).toEqual(stringTest);
 });
