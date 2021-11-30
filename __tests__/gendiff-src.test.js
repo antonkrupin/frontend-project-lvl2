53ae420848007f16a553e2932a05e5a8588e05cd
Anton Krupin <krupin_anton@mail.ru>
2021-11-30 12:20:10 +0300

    change test

--- __tests__/gendiff-src.test.js
+++ __tests__/gendiff-src.test.js
@@ -4,9 +4,8 @@ import path from 'path';
 test('gendiffString_partialNonEqualFiles', () => {
     const path1 = 'file1.json';
     const path2 = 'file2.json';
-    const getFixturePath = (filename) => path.join('__fixtures__', filename);
+    const getFixturePath = (filename) => path.join('..', '__fixtures__', filename);
     const file1 = getFixturePath(path1);
-    console.log(file1)
     const file2 = getFixturePath(path2);
     const test = ['- follow: false', '  host: hexlet.io', '- proxy: 123.234.53.22', '- timeout: 50', '+ timeout: 20', '+ verbose: true'].join('\n')
     expect(gendiffString(file1, file2).join('\n')).toEqual(test);
