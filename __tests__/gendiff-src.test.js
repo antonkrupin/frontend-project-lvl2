b848f8360fc529995b154462b71269b3c29cafa3
Anton Krupin <krupin_anton@mail.ru>
2021-11-30 10:25:26 +0300

    change test

--- __tests__/gendiff-src.test.js
+++ __tests__/gendiff-src.test.js
@@ -2,15 +2,15 @@ import gendiffString from "../src/gendiff-src.js";
 import path from 'path';
 
 test('gendiffString', () => {
-    const path1 = 'file1.json';
-    const path2 = 'file2.json';
+    const path1 = 'file3.json';
+    const path2 = 'file4.json';
     const getFixturePath = (filename) => path.join('..', '__fixtures__', filename);
     const file1 = getFixturePath(path1);
     const file2 = getFixturePath(path2);
     const test = ['- follow: false', '  host: hexlet.io', '- proxy: 123.234.53.22', '- timeout: 50', '+ timeout: 20', '+ verbose: true'].join('\n')
     expect(gendiffString(file1, file2).join('\n')).toEqual(test);
 });
-
+/*
 test('gendiffString', () => {
     const path1 = 'file3.json';
     const path2 = 'file4.json';
@@ -19,4 +19,4 @@ test('gendiffString', () => {
     const file2 = getFixturePath(path2);
     const test = ['  host: hexlet.io', '  timeout: 50', '  proxy: 123.234.53.22', '  follow: false'];
     expect(gendiffString(file1, file2).join('\n')).toEqual(test);
-});
+});*/
