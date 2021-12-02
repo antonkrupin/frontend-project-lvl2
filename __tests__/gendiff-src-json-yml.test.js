b661aeaf705a7e94b598b1780659fb693ef0f017
Anton Krupin <krupin_anton@mail.ru>
2021-12-02 11:00:21 +0300

    change

--- __tests__/gendiff-src-json-yml.test.js
+++ __tests__/gendiff-src-json-yml.test.js
@@ -4,7 +4,7 @@ import path from 'path';
 test('gendiffString_partialNonEqualFiles', () => {
     const path1 = 'file1.yml';
     const path2 = 'file2.json';
-    const getFixturePath = (filename) => path.join('..', '__fixtures__', filename);
+    const getFixturePath = (filename) => path.join('.', '__fixtures__', filename);
     const file1 = getFixturePath(path1);
     const file2 = getFixturePath(path2);
     const test = ['- follow: false', '  host: hexlet.io', '- proxy: 123.234.53.22', '- timeout: 50', '+ timeout: 20', '+ verbose: true'].join('\n')
@@ -14,7 +14,7 @@ test('gendiffString_partialNonEqualFiles', () => {
 test('gendiffString_equalFiles', () => {
     const path1 = 'equalFile1.json';
     const path2 = 'equalFile2.yml';
-    const getFixturePath = (filename) => path.join('..', '__fixtures__', filename);
+    const getFixturePath = (filename) => path.join('.', '__fixtures__', filename);
     const file1 = getFixturePath(path1);
     const file2 = getFixturePath(path2);
     const test = ['  follow: false', '  host: hexlet.io', '  proxy: 123.234.53.22', '  timeout: 50'].join('\n');
@@ -24,7 +24,7 @@ test('gendiffString_equalFiles', () => {
 test('gendiffString_fullNonEqualFiles', () => {
     const path1 = 'fullNonEqualFile1.yml';
     const path2 = 'fullNonEqualFile2.yml';
-    const getFixturePath = (filename) => path.join('..', '__fixtures__', filename);
+    const getFixturePath = (filename) => path.join('.', '__fixtures__', filename);
     const file1 = getFixturePath(path1);
     const file2 = getFixturePath(path2);
     const test = ['+ follow: false', '+ proxy: 123.234.53.22', '- timeout: 20', '- verbose: true'].join('\n')
