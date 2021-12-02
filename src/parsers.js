2cd33b90a2583f19c637db6ae7ed9f2ee97df7a3
Anton Krupin <krupin_anton@mail.ru>
2021-12-02 11:10:45 +0300

    change getFileExtension function

--- src/parsers.js
+++ src/parsers.js
@@ -3,8 +3,7 @@ import { readFileSync } from 'fs';
 import path from 'path';
 
 const getFileExtension = (fileName) => {
-    //return path.extname(fileName);
-    return fileName.split('.').pop();
+    return path.extname(fileName);
 };
 
 const readFile = (pathToFile) => {
@@ -17,10 +16,10 @@ const readFile = (pathToFile) => {
 };
 
 const parseFile = (file, fileExtension) => {
-    if (fileExtension === 'json') {
+    if (fileExtension === '.json') {
         return JSON.parse(readFile(file));
     }
-    if (fileExtension === 'yml' || fileExtension === 'yaml') {
+    if (fileExtension === '.yml' || fileExtension === '.yaml') {
         return _.load(readFile(file));
     }
 };
