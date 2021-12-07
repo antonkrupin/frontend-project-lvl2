5b8d3920bc6efb8fe6d03fd12f51716c56da6197
Anton Krupin <krupin_anton@mail.ru>
2021-12-07 12:47:24 +0300

    add getAllKeys function

--- src/gendiff-src.js
+++ src/gendiff-src.js
@@ -13,20 +13,32 @@ const findDiff = (sortedKeys, file1JSON, file2JSON) => sortedKeys.map((el) => {
   }
   return (`+ ${el}: ${file2JSON[el]}`);
 });
-/*
-    проверяем свойства
-    если свойство простое то идем как обычно
-    если свойство объект, то то идем внутрь его
-    */
+
+const getAllKeys = (obj) => {
+  const array = [];
+  const getKeys = (o) => {
+    const keys = Object.keys(o);
+    // eslint-disable-next-line no-restricted-syntax
+    for (const key in keys) {
+      if (_.isObject(o[keys[key]])) {
+        array.push(keys[key]);
+        getKeys(o[keys[key]]);
+      } else {
+        array.push(keys[key]);
+      }
+    }
+  };
+  getKeys(obj);
+  return array;
+};
 
 const gendiffString = (path1, path2) => {
   const parsedFile1 = parseFile(path1, getFileExtension(path1));
   const parsedFile2 = parseFile(path2, getFileExtension(path2));
 
-  console.log(parsedFile1);
-  console.log(parsedFile2);
-
   const sortedKeysUnion = _.union(Object.keys(parsedFile1), Object.keys(parsedFile2)).sort();
+
+  console.log(sortedKeysUnion);
 
   return findDiff(sortedKeysUnion, parsedFile1, parsedFile2);
 };
