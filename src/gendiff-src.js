b89982845c86c69be64dee157300f955421dc056
Anton Krupin <krupin_anton@mail.ru>
2022-01-12 14:48:49 +0300

    made something

--- src/gendiff-src.js
+++ src/gendiff-src.js
@@ -29,28 +29,63 @@ const getAllKeys = (obj) => {
 
 const showKeysValues = (keysArray, obj1, obj2) => {
   const test = keysArray.map((e) => {
-    if (_.isObject(e)) {
+    if (!_.isObject(e)) {
+      console.log('_____');
+      console.log(e);
+      console.log('______');
       return 1;
     }
+    console.log(e);
     return 0;
   });
   return test;
+};
+
+const functionForNested = (keys, obj1, obj2) => {
+  const isItObject = (key) => {
+    const testObj = {};
+    if (_.isObject(obj2[key])) {
+      testObj[key] = obj2[key];
+      //console.log('_____');
+      //console.log(testObj);
+      const keys = _.keys(testObj[key]);
+    }
+    return findDiff([key], obj1, obj2);
+  };
+
+  const test = keys.map((el) => {
+    /*if (_.has(obj1, el) && _.has(obj2, el)) {
+      return (` ${el} : ${isItObject(el)}`);
+    }*/
+    isItObject(el);
+  });
+  return test;
+  /*
+  проверяем значения по ключам
+  если по ключу объект - запускаем снова и запоминаем
+  если по ключу простое значение - возвращаем
+  */
 };
 
 const gendiffString = (path1, path2) => {
   const parsedFile1 = parseFile(path1, getFileExtension(path1));
   const parsedFile2 = parseFile(path2, getFileExtension(path2));
 
-  const sortedKeysUnion = _.union(getAllKeys(parsedFile1), getAllKeys(parsedFile2)).sort();
+  const sortedKeysUnion = _.flattenDeep(_.union(getAllKeys(parsedFile1), getAllKeys(parsedFile2)));
+  const uniqSortedKeysUnion = _.uniq(sortedKeysUnion.sort());
+  // console.log(uniqSortedKeysUnion);
 
-  const test = _.merge(getAllKeys(parsedFile1), getAllKeys(parsedFile2)).flat();
-  console.log(test);
-  console.log(test.length);
+  console.log('__________');
+  console.log(functionForNested(uniqSortedKeysUnion, parsedFile1, parsedFile2));
+  console.log('____________');
+  // const test = _.merge(getAllKeys(parsedFile1), getAllKeys(parsedFile2)).flat();
+  // console.log(test);
+  // console.log(test.length);
   // console.log(sortedKeysUnion);
 
-  const test1 = showKeysValues(sortedKeysUnion, parsedFile1, parsedFile2);
+  // const test1 = showKeysValues(test, parsedFile1, parsedFile2);
 
-  console.log(test1);
+  // console.log(test1);
 
   return findDiff(sortedKeysUnion, parsedFile1, parsedFile2);
 };
