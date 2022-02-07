55dc5077f971b424823268fc775d5806ae2d29d5
Anton Krupin <krupin_anton@mail.ru>
2022-02-07 10:39:03 +0300

    add options in command

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -12,11 +12,11 @@ program
   .description('Compares two configuration files and shows a difference')
   .option('-V, --version', 'output the version number')
   .helpOption('-h, --help', 'output usage information')
-  .option('-f, --format', 'output format')
+  .option('-f, --format [type]', 'output format', 'stylish')
   .argument('<pathToFile1>', 'enter path to file 1')
   .argument('<pathToFile2>', 'enter path to file 2')
-  .action((pathToFile1, pathToFile2) => {
-    const diff = gendiffString(pathToFile1, pathToFile2);
+  .action((pathToFile1, pathToFile2, options) => {
+    const diff = gendiffString(pathToFile1, pathToFile2, options.format);
     console.log(diff);
   });
 
