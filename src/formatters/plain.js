1a6b8322365a3b2e712f46eb7a447cfb6ef776a4
Anton Krupin <krupin_anton@mail.ru>
2022-02-07 13:17:03 +0300

    change function createString

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -7,11 +7,25 @@ const checkValue = (value) => {
 };
 
 const createString = (data, previousNode) => {
-  
+  const stringTemplate = `Property ${previousNode.join('.')} was ${data.status}`;
+
+  if (data.status === 'added') {
+    return `${stringTemplate} with value: ${checkValue(data.value)}`;
+  }
+
+  if (data.status === 'removed') {
+    return `${stringTemplate}`;
+  }
+
+  if (data.status === 'updated') {
+    return `${stringTemplate}. From ${checkValue(data.value1)} to ${checkValue(data.value2)}`;
+  }
+
+  return [];
 };
 
 const plain = (diff) => {
-
+  const recursion
 };
 
 export default plain;
