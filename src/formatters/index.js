6f63a6b9bac1f0c476399d3aa32f0d5183ef126d
Anton Krupin <krupin_anton@mail.ru>
2022-02-07 13:28:12 +0300

    add switch to formattedDiff function

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,6 +1,15 @@
-import plain from "./plain.js";
-import stylish from "./stylish.js";
+import plain from './plain.js';
+import stylish from './stylish.js';
 
+// eslint-disable-next-line consistent-return
 const formattedDiff = (diff, formatType) => {
-
+  // eslint-disable-next-line default-case
+  switch (formatType) {
+    case 'stylish':
+      return stylish(diff);
+    case 'plain':
+      return plain(diff);
+  }
 };
+
+export default formattedDiff;
