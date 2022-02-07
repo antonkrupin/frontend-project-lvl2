0503c9700c3cf999a357c20b8c7e88e427ac4975
Anton Krupin <krupin_anton@mail.ru>
2022-02-07 09:33:30 +0300

    remove functions findDiff, getAllKeys

--- src/gendiff-src.js
+++ src/gendiff-src.js
@@ -2,32 +2,6 @@ import _ from 'lodash';
 import { getFileExtension, parseFile } from './parsers.js';
 import createDiff from './createDiff.js';
 
-const findDiff = (sortedKeys, file1JSON, file2JSON) => sortedKeys.map((el) => {
-  if (_.has(file1JSON, el) && _.has(file2JSON, el)) {
-    if (file1JSON[el] === file2JSON[el]) {
-      return (`  ${el}: ${file1JSON[el]}`);
-    }
-    return ([(`- ${el}: ${file1JSON[el]}`), (`+ ${el}: ${file2JSON[el]}`)].join('\n'));
-  }
-  if (_.has(file1JSON, el)) {
-    return (`- ${el}: ${file1JSON[el]}`);
-  }
-  return (`+ ${el}: ${file2JSON[el]}`);
-});
-
-const getAllKeys = (obj) => {
-  const getKeys = (o) => {
-    const keys = Object.keys(o);
-    return keys.map((key) => {
-      if (_.isObject(o[key])) {
-        return [key, getKeys(o[key])];
-      }
-      return key;
-    });
-  };
-  return getKeys(obj);
-};
-
 export const stringify = (data, replacer = ' ', indentCount = 1, depth = 1) => {
   const setIndent = (treeDepth) => replacer.repeat(indentCount * treeDepth);
 
@@ -40,16 +14,6 @@ export const stringify = (data, replacer = ' ', indentCount = 1, depth = 1) => {
   return ['{', ...lines, `${setIndent(depth - 1)}}`].join('\n');
 };
 
-const gendiffString = (path1, path2) => {
-  const parsedFile1 = parseFile(path1, getFileExtension(path1));
-  const parsedFile2 = parseFile(path2, getFileExtension(path2));
-
-  const sortedKeysUnion = _.flattenDeep(_.union(getAllKeys(parsedFile1), getAllKeys(parsedFile2)));
-  const uniqSortedKeysUnion = _.uniq(sortedKeysUnion.sort());
-
-  return findDiff(uniqSortedKeysUnion, parsedFile1, parsedFile2);
-};
-
 const genDiffString = (path1, path2) => {
   const parsedFile1 = parseFile(path1, getFileExtension(path1));
   const parsedFile2 = parseFile(path2, getFileExtension(path2));
@@ -58,4 +22,4 @@ const genDiffString = (path1, path2) => {
   return diff;
 };
 
-export default gendiffString;
+export default genDiffString;
