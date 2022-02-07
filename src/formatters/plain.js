d25275cd77546e72c5d7f5cbb399ec18a73a4e7f
Anton Krupin <krupin_anton@mail.ru>
2022-02-07 13:34:46 +0300

    change checkValue function

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,8 +1,12 @@
 import _ from 'lodash';
 
 const checkValue = (value) => {
-  if (_.isObject(value)) return '[complex value]';
-  if (typeof (value) === 'string') return `${value}`;
+  if (_.isObject(value)) {
+    return '[complex value]';
+  }
+  if (typeof (value) === 'string') {
+    return `${value}`;
+  }
   return value;
 };
 
