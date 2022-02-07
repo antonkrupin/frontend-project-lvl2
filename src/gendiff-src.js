27e305e1aa2b3cd9f6c70ceae545167cfac71994
Anton Krupin <krupin_anton@mail.ru>
2022-02-07 13:31:26 +0300

    remove import stylish from './formattesr/stylish.js'

--- src/gendiff-src.js
+++ src/gendiff-src.js
@@ -1,13 +1,13 @@
 import { getFileExtension, parseFile } from './parsers.js';
-import stylish from './formatters/stylish.js';
 import createDiff from './createDiff.js';
+import formattedDiff from './formatters/index.js';
 
-const genDiffString = (path1, path2) => {
+const genDiffString = (path1, path2, formatType = 'stylish') => {
   const parsedFile1 = parseFile(path1, getFileExtension(path1));
   const parsedFile2 = parseFile(path2, getFileExtension(path2));
 
   const diff = createDiff(parsedFile1, parsedFile2);
-  return stylish(diff);
+  return formattedDiff(diff, formatType);
 };
 
 export default genDiffString;
