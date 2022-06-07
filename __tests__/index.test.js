cd0542440257e01f32d6168c8252b6f66ace7389
Anton Krupin <krupin_anton@mail.ru>
2022-06-07 10:16:14 +0300

    change test file

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,6 +1,45 @@
-import path from 'path';
+import fs from 'fs';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
 import gendiffString from '../src/index.js';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
+
+const formats = ['json', 'yml'];
+
+const resultStylish = readFile('result_stylish.txt');
+const resultPlain = readFile('result_plain.txt');
+const resultJson = readFile('result_json.json');
+
+test.each(formats)('different formats of files %s', (format) => {
+  const filePath1 = getFixturePath(`file1.${format}`);
+  const filePath2 = getFixturePath(`file2.${format}`);
+
+  expect(gendiffString(filePath1, filePath2)).toBe(resultStylish);
+
+  expect(gendiffString(filePath1, filePath2, 'stylish')).toBe(resultStylish);
+  expect(gendiffString(filePath1, filePath2, 'plain')).toBe(resultPlain);
+  expect(gendiffString(filePath1, filePath2, 'json')).toBe(resultJson);
+
+  expect(() => JSON.parse(gendiffString(filePath1, filePath2, 'json'))).not.toThrow();
+  expect(() => gendiffString(filePath1, filePath2, 'jsone')).toThrow();
+});
+
+const wrongFormats = ['txt'];
+
+test.each(wrongFormats)('wrong formats of files %s', (format) => {
+  const filePath1 = getFixturePath(`file1.${format}`);
+  const filePath2 = getFixturePath(`file2.${format}`);
+
+  expect(() => gendiffString(filePath1, filePath2)).toThrow();
+});
+
+/*
 test('gendiffString_partialNonEqualFiles', () => {
   const path1 = 'file1.json';
   const path2 = 'file2.json';
@@ -39,4 +78,4 @@ test('gendiffString_PlainFormatter_FullNonEqualFiles', () => {
   const file2 = getFixturePath(path2);
   const test = ['Property follow was added with value: false', 'Property proxy was added with value: 123.234.53.22', 'Property timeout was removed', 'Property verbose was removed'].join('\n');
   expect(gendiffString(file1, file2, 'plain')).toEqual(test);
-});
+}); */
