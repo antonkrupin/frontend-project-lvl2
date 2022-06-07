c4aea4a14c2669ecf1f1e86f5d0746e3db643710
Anton Krupin <krupin_anton@mail.ru>
2022-06-07 09:49:03 +0300

    replace if statment with switch statement

--- src/parsers.js
+++ src/parsers.js
@@ -1,10 +1,17 @@
 import _ from 'js-yaml';
 
 const parseFile = (file, fileExtension) => {
-  if (fileExtension === '.yml' || fileExtension === '.yaml') {
+  /* if (fileExtension === '.yml' || fileExtension === '.yaml') {
     return _.load(file);
   }
-  return JSON.parse(file);
+  return JSON.parse(file); */
+
+  switch (fileExtension) {
+    case 'yml' || 'yaml':
+      return _.load(file);
+    default:
+      return JSON.parse(file);
+  }
 };
 
 export default parseFile;
