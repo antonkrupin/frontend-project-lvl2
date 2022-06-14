ca9ea9b456e1a3d80d7cf349a0477efa55de58f1
Anton Krupin <krupin_anton@mail.ru>
2022-06-14 21:19:49 +0300

    remove comments. add default case in switch

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -2,9 +2,7 @@ import stylish from './stylish.js';
 import plain from './plain.js';
 import json from './json.js';
 
-// eslint-disable-next-line consistent-return
 const formattedDiff = (diff, formatType) => {
-  // eslint-disable-next-line default-case
   switch (formatType) {
     case 'stylish':
       return stylish(diff);
@@ -12,6 +10,8 @@ const formattedDiff = (diff, formatType) => {
       return plain(diff);
     case 'json':
       return json(diff);
+    default:
+      throw new Error('Wrong Formatter type. Use - stylish, plain or json');
   }
 };
 
