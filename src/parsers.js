b3870d665f498e2a08c3ea48e18c63a7c4129f16
Anton Krupin <krupin_anton@mail.ru>
2022-06-14 21:16:56 +0300

    add case for 'json' format. in default case add throw error

--- src/parsers.js
+++ src/parsers.js
@@ -4,8 +4,10 @@ const parseFile = (file, fileExtension) => {
   switch (fileExtension.slice(1)) {
     case 'yml' || 'yaml':
       return _.load(file);
-    default:
+    case 'json':
       return JSON.parse(file);
+    default:
+      throw new Error('Wrong file format. Use json or yaml format.');
   }
 };
 
