cd57d6d02c17029c679d714debd6efa4f30d0cb2
Anton Krupin <krupin_anton@mail.ru>
2022-06-14 21:13:09 +0300

    remove comments

--- src/parsers.js
+++ src/parsers.js
@@ -1,11 +1,6 @@
 import _ from 'js-yaml';
 
 const parseFile = (file, fileExtension) => {
-  /* if (fileExtension === '.yml' || fileExtension === '.yaml') {
-    return _.load(file);
-  }
-  return JSON.parse(file); */
-
   switch (fileExtension.slice(1)) {
     case 'yml' || 'yaml':
       return _.load(file);
